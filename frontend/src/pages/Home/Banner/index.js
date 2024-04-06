fe23327a121a4d74b3e5e5de079f11c45a486b28
LeHung560 <hungx5qt2002@gmail.com>
2024-04-06 20:33:44 +0700

    update

--- frontend/src/pages/Home/Banner/index.js
+++ frontend/src/pages/Home/Banner/index.js
@@ -44,116 +44,100 @@ const Banner = () => {
     });
   };
   return (
-    <div className="">
-      <div className={`relative mt-10`}>
-        <div className={` ${styles.searchChoose}`}>
-          <nav>
-            <ul className={styles.searchChooseNav}>
-              <li
-                onClick={() => {
-                  handleActive("tour");
-                }}
-                className={`${styles.searchChooseNavLi} ${
-                  active.tour ? "bg-white text-2d4271" : "text-white bg-inherit"
-                } font-bold`}
+    <div className={`calc-64 bg-F5F7FA flex flex-col justify-center items-center ${styles.main}`}>
+      <div className="mb-14">
+        <h1 className="font-semibold text-center text-4D4D4D text-7xl">
+          Du lịch khắp mọi nơi!
+        </h1>
+        <p className="text-center text-717171 text-3xl">
+          Cung cấp cho bạn những trải nghiệm tuyệt vời.
+        </p>
+      </div>
+      <div className={`h-max py-3 border-b-2 border-4CAF4F`}>
+        <div className={`flex justify-around items-center`}>
+          <div className=" flex items-center justify-around py-1 px-5 border-2 border-yellow-200 rounded-2xl h-20">
+            <i className="fa-solid fa-location-dot mr-2 text-red-400 text-xl"></i>
+            <div>
+              <label
+                htmlFor="countriesFrom"
+                className=" ml-4 mb-2 text-sm font-medium"
               >
-                Tour du lịch
-              </li>
-            </ul>
-          </nav>
-          <div className={styles.searchChooseContent}>
-            {active.tour && (
-              <div className="h-40">
-                <p className={styles.searchChooseP}>Tìm kiếm tour phù hợp</p>
-                <div className={styles.searchChooseFilter}>
-                  <div className=" flex items-center justify-around py-1 px-5 border-4 border-yellow-200 rounded-2xl h-20">
-                    <i className="fa-solid fa-location-dot mr-2"></i>
-                    <div>
-                      <label
-                        htmlFor="countriesFrom"
-                        className=" ml-4 mb-2 text-sm font-medium"
-                      >
-                        Điểm đi
-                      </label>
-                      <select
-                        value={countryFrom}
-                        onChange={handleFrom}
-                        id="countriesFrom"
-                        className="bg-gray-50 outline-none border-b border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                      >
-                        <option value="">Chọn địa phương</option>
-                        {cityLabels.map((item, index) => (
-                          <option key={index} value={item.value}>
-                            {item.label}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
-                  </div>
-                  <i className={`fa-solid fa-right-left`}></i>
-                  <div className=" flex items-center justify-between py-1 px-7 border-4 border-yellow-200 rounded-2xl h-20">
-                    <i className="fa-solid fa-location-dot mr-2"></i>
-                    <div>
-                      <label
-                        htmlFor="countriesTo"
-                        className="mb-2 ml-4 text-sm font-medium"
-                      >
-                        Điểm đến
-                      </label>
-                      <select
-                        value={countryTo}
-                        onChange={handleTo}
-                        id="countriesTo"
-                        className="bg-gray-50 outline-none border-b border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                      >
-                        <option value="">Chọn địa phương</option>
-                        {cityLabels.map((item, index) => (
-                          <option key={index} value={item.value}>
-                            {item.label}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
-                  </div>
-                  <div className="py-1 px-7 border-4 border-yellow-200 rounded-2xl h-20">
-                    <div>
-                      <span>Ngày đi</span>
-                    </div>
-                    <div className="h-18">
-                      <DatePicker onChange={onChange} value={date} />
-                    </div>
-                  </div>
-                  <div className=" flex items-center justify-around py-1 px-7 border-4 border-yellow-200 rounded-2xl h-20">
-                    <div>
-                      <label
-                        htmlFor="number-input"
-                        className="mb-2 text-sm font-medium"
-                      >
-                        Số ngày
-                      </label>
-                      <input
-                        onChange={handleNumberDate}
-                        value={numberDate}
-                        type="number"
-                        id="number-input"
-                        aria-describedby="helper-text-explanation"
-                        className="bg-gray-50 text-sm rounded-lg
+                Điểm đi
+              </label>
+              <select
+                value={countryFrom}
+                onChange={handleFrom}
+                id="countriesFrom"
+                className="text-404040 bg-gray-50 outline-none border-b border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              >
+                <option value="">Chọn địa phương</option>
+                {cityLabels.map((item, index) => (
+                  <option key={index} value={item.value}>
+                    {item.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <i className={`text-404040 mx-4 fa-solid fa-right-left`}></i>
+          <div className=" flex items-center justify-between py-1 px-7 border-2 border-yellow-200 rounded-2xl h-20">
+            <i className="fa-solid fa-location-dot mr-2 text-red-400 text-xl text-base"></i>
+            <div>
+              <label
+                htmlFor="countriesTo"
+                className="mb-2 ml-4 text-sm font-medium"
+              >
+                Điểm đến
+              </label>
+              <select
+                value={countryTo}
+                onChange={handleTo}
+                id="countriesTo"
+                className="text-404040 bg-gray-50 outline-none border-b border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              >
+                <option value="">Chọn địa phương</option>
+                {cityLabels.map((item, index) => (
+                  <option key={index} value={item.value}>
+                    {item.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="py-1 px-7 mx-4 border-2 border-yellow-200 rounded-2xl h-20">
+            <div>
+              <span>Ngày đi</span>
+            </div>
+            <div className="h-18 text-404040">
+              <DatePicker onChange={onChange} value={date} />
+            </div>
+          </div>
+          {/* <div className=" flex items-center justify-around py-1 px-7 border-4 border-yellow-200 rounded-2xl h-20">
+            <div>
+              <label
+                htmlFor="number-input"
+                className="mb-2 text-sm font-medium"
+              >
+                Số ngày
+              </label>
+              <input
+                onChange={handleNumberDate}
+                value={numberDate}
+                type="number"
+                id="number-input"
+                aria-describedby="helper-text-explanation"
+                className="bg-gray-50 text-sm rounded-lg
                    focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5"
-                        placeholder="0"
-                        min={0}
-                      />
-                    </div>
-                  </div>
-                  <div
-                    onClick={handleClick}
-                    className="bg-yellow-300 py-4 px-5 rounded-lg cursor-pointer"
-                  >
-                    <i className={`fa-solid fa-right-long text-white`}></i>
-                  </div>
-                </div>
-              </div>
-            )}
-            {active.hotel && <div className="h-40"></div>}
+                placeholder="0"
+                min={0}
+              />
+            </div>
+          </div> */}
+          <div
+            onClick={handleClick}
+            className="bg-yellow-300 py-4 px-5 rounded-lg cursor-pointer"
+          >
+            <p className="font-semibold text-404040">Tìm kiếm</p>
           </div>
         </div>
       </div>
