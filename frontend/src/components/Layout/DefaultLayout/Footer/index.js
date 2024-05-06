78366946144e7f69ef47389fc88da12f982c74c7
LeHung560 <hungx5qt2002@gmail.com>
2024-05-06 13:21:38 +0700

    update

--- frontend/src/components/Layout/DefaultLayout/Footer/index.js
+++ frontend/src/components/Layout/DefaultLayout/Footer/index.js
@@ -1,55 +1,42 @@
+import Icons from "../../../Icons";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 m-0">
-      <div className="flex flex-wrap pt-8 justify-between mx-24 my-0">
-        <div>
-          <p className="font-semibold text-2xl leading-8 text-white mb-5">
-            Giới thiệu
-          </p>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Về công ty
+    <footer>
+      <div className="flex flex-col space-y-5 py-5  justify-center w-11/12 mx-auto border-t border-dfe3e6">
+        <div className="flex justify-center space-x-5">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icons.FaceBook />
           </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Tuyển dụng
+          <a
+            href="https://messenger.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icons.Messenger />
           </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Hệ thống cửa hàng
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icons.Instagram />
           </a>
-        </div>
-        <div>
-          <p className="font-semibold text-2xl leading-8 text-white mb-5">
-            Dịch vụ khách hàng
-          </p>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Chính sách điều khoản
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Hướng dẫn
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Q&A
-          </a>
-        </div>
-        <div>
-          <p className="font-semibold text-2xl leading-8 text-white mb-5">
-            Liên hệ
-          </p>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Hotline
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Email
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Live chat
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Messenger
-          </a>
-          <a className="block text-sm leading-6 text-gray-400 mb-4 no-underline">
-            Liên hệ
+          <a
+            href="https://youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icons.Youtube />
           </a>
         </div>
+        <p className="text-center text-gray-700 font-medium">
+          Copyright &copy; 2024 Company Ltd.
+        </p>
       </div>
     </footer>
   );
